757ea6716506d10d16f6cd731662da164a78b570
tyrone.v <tyrone.vgt@gmail.com>
2020-10-06 12:24:13 +0100

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React_Native_Okta_Auth
---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oa6sl23ZPTAGzW7s5d5',
+    redirectUri: 'com.okta.dev-732758:/callback',
+    endSessionRedirectUri: 'com.okta.dev-732758:/callback',
+    discoveryUri: 'https://dev-732758.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
